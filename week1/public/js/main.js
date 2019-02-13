c602e07989b937a19ccf2e4428f70172855cfa7f
MitchGoudkuil <mitch_goudkuil@hotmail.com>
2019-02-13 11:00:25 +0100

    added salad part, now clickable

--- week1/public/js/main.js
+++ week1/public/js/main.js
@@ -1,10 +1,22 @@
 let fries = document.querySelector('.header-navigation-item__fries');
 let drink = document.querySelector('.header-navigation-item__drink');
 let steak = document.querySelector('.header-navigation-item__steak');
+let salad = document.querySelector('.header-navigation-item__bowl');
+
 
 let fryBackground = document.querySelector('.background-containers__fry');
 let drinkBackground = document.querySelector('.background-containers__drink');
 let steakBackground = document.querySelector('.background-containers__steak');
+let saladBackground = document.querySelector('.background-containers__salad');
+
+salad.addEventListener("click", function(){
+  saladBackground.classList.toggle("nowidth")
+  saladBackground.style.zIndex = 2
+
+  drinkBackground.classList.add("nowidth")
+  fryBackground.classList.add("nowidth")
+  steakBackground.classList.add("nowidth")
+})
 
 fries.addEventListener("click", function(){
   fryBackground.classList.toggle("nowidth")
@@ -12,6 +24,7 @@ fries.addEventListener("click", function(){
 
   drinkBackground.classList.add("nowidth")
   steakBackground.classList.add("nowidth")
+  saladBackground.classList.add("nowidth")
 })
 
 drink.addEventListener("click", function(){
@@ -19,6 +32,8 @@ drink.addEventListener("click", function(){
   drinkBackground.style.zIndex = 2
 
   fryBackground.classList.add("nowidth")
+  steakBackground.classList.add("nowidth")
+    saladBackground.classList.add("nowidth")
 })
 
 steak.addEventListener("click", function(){
@@ -27,4 +42,5 @@ steak.addEventListener("click", function(){
 
   drinkBackground.classList.add("nowidth")
   fryBackground.classList.add("nowidth")
+  saladBackground.classList.add("nowidth")
 })
